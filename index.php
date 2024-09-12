7770dc4dfe94ab4d6ba4f67a8dcfe0fc19de7e0f
softgrowthinfotech01 <softgrowthinfotech@gmail.com>
2024-09-12 14:31:48 +0530

    index

--- index.php
+++ index.php
@@ -54,6 +54,8 @@
  .carousel-item img{
   height:100%;
  }
+
+
 </style>
 <body>
   <div class="hero_area">
@@ -73,7 +75,7 @@
     <!-- end header section -->
     <!-- slider section -->
     <section class=" slider_section position-relative">
-    <div id="ImageCarouselCSS" class="carousel slide container-fluid px-0"  data-ride="carousel">
+    <div id="ImageCarouselCSS" class="carousel slide container-fluid px-0 carousel-fade"  data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#ImageCarouselCSS" data-slide-to="0" class="active"></li>
     <li data-target="#ImageCarouselCSS" data-slide-to="1"></li>
@@ -81,11 +83,11 @@
     <li data-target="#ImageCarouselCSS" data-slide-to="3"></li>
     <li data-target="#ImageCarouselCSS" data-slide-to="4"></li>
   </ol>
-  <div class="carousel-inner">
+  <div class="carousel-inner"  data-aos="fade-right">
     <div class="carousel-item active">
       <img src="images/mutual_1.png" class="d-block img img-fluid w-100 height: 538px;" alt="m_fund">
     </div>
-    <div class="carousel-item">
+    <div class="carousel-item" >
       <img src="images/fs.png" class="d-block img img-fluid w-100" alt="finance">
     </div>
     <div class="carousel-item">
